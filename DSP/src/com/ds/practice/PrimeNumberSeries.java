c81a83975acb986f5aa7d409bb5bd344a5d1d2d8
raman699 <raman.699@rediff.com>
2018-07-01 13:42:17 +0530

    Prime number commit

---
+++ DSP/src/com/ds/practice/PrimeNumberSeries.java
@@ -0,0 +1,30 @@
+package com.ds.practice;
+
+import java.util.Scanner;
+
+public class PrimeNumberSeries {
+
+	public static void main(String[] args) {
+		int n=(new Scanner(System.in)).nextInt();
+        boolean[] notPrime = new boolean[n];
+
+        for(int i=2;i<n;i++)
+        {
+        	if(!notPrime[i])
+        	{
+        		for(int j=2*i;j<n;j=j+i)
+        		{
+        			notPrime[j]=true;
+        		}
+        	}
+        }
+        for(int i=0;i<n;i++)
+        {
+        	if(!notPrime[i])
+        	{
+        		System.out.println(i);
+        	}
+        	}
+        }
+	}
+
